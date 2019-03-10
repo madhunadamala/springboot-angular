540c62b36b0622421f2a4e35bb0ced10ac3b97c0
MADHU NADAMALA <madhunadamala@MADHUs-MacBook-Air.local>
2019-03-09 22:53:45 -0800

    aaded

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
-import { ViewRgistrationComponent } from './components/view-rgistration/view-rgistration.component';
-
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 const routes: Routes = [
   {
